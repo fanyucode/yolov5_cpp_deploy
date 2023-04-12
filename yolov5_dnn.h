8a742d8657f416e4a186c90133fc27df180d13a8
宇 范 <2272115338@qq.com>
2023-04-12 23:42:11 +0800

    yolov5_dnn.h

---
+++ yolov5_dnn.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <opencv2/opencv.hpp>
+
+struct DetectResult {
+	int classId;
+	float score;
+	cv::Rect box;
+};
+
+class YOLOv5DNNDetector {
+public:
+	void initConfig(std::string onnxpath, int iw, int ih, float threshold);
+	void detect(cv::Mat & frame, std::vector<DetectResult> &result);
+private:
+	int input_w = 640;
+	int input_h = 640;
+	cv::dnn::Net net;
+	int threshold_score = 0.25;
+};
